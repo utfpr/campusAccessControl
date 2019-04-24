c40ef7a74c9936c00ed7842dfa08a67700d7096f
Guilherme Canesin <40005746+Guilherme-Canesin@users.noreply.github.com>
2019-04-24 19:46:46 -0300

    Delete config.js

--- back-end/src/config.js
+++
@@ -1,8 +0,0 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef'; 
-global.EMAIL_TMPL = '<strong>{0}</strong>';   
-  
-module.exports = { 
-     connectionString: '', 
-     sendgriidKey: 'TBD', 
-     containerConnectionString: 'TBD'
- }
