d89dc9918b98258647e28db2378053e4dbbba0cb
Alisson Vieira <alisson.v3@hotmail.com>
2019-09-24 17:29:41 -0300

    Alteracoes na listagem
    binbin

--- front-end/src/components/users-list.component.js
+++ front-end/src/components/users-list.component.js
@@ -1,74 +1,153 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
+// import React, {Component} from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+
+// const User = props => (
+//     <tr>
+//         <td className={props.user.user_completed ? 'completed' : ''}>{props.user.user_name}</td>
+//         <td className={props.user.user_completed ? 'completed' : ''}>{props.user.user_email}</td> 
+//         <td className={props.user.user_completed ? 'completed' : ''}>{props.user.user_password}</td> 
+//         <td className={props.user.user_completed ? 'completed' : ''}>{props.user.user_type}</td>
+//         <td>
+//             <Link to={"/edituser/"+props.user._id}>Edit</Link>
+//         </td> 
+//         <td>
+//             <Link to={"/delete/"+props.user._id}>Delete</Link> 
+//         </td>
+//     </tr>
+// )
+
+// export default class UsersList extends Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {users: []};
+//     }
+
+//     componentDidMount() {
+//         axios.get('http://localhost:4000/users/')
+//             .then(response => {
+//                 this.setState({users: response.data});
+//             })
+//             .catch(function (error) {
+//                 console.log(error);
+//             })
+//     }
+
+//     componentDidUpdate() {
+//         axios.get('http://localhost:4000/users/')
+//         .then(response => {
+//             this.setState({users: response.data});
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         })   
+//     }
+
+//     userList() {
+//         return this.state.users.map(function(currentUser, i) {
+//             return <User user={currentUser} key={i} />;
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h3>Lista de usuários</h3>
+//                 <table className="table table-warning table-striped" style={{ marginTop: 20 }}>
+//                     <thead>
+//                         <tr>
+//                             <th>Username</th>
+//                             <th>Email</th> 
+//                             <th>Password</th> 
+//                             <th>Type</th>
+//                             <th>Actions</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         { this.userList() }
+//                     </tbody>
+//                 </table>
+//             </div>
+//         )
+//     }
+// }
+
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { 
+    Table,
+    Icon
+} from 'antd';
 
-const User = props => (
-    <tr>
-        <td className={props.user.user_completed ? 'completed' : ''}>{props.user.user_name}</td>
-        <td className={props.user.user_completed ? 'completed' : ''}>{props.user.user_email}</td> 
-        <td className={props.user.user_completed ? 'completed' : ''}>{props.user.user_password}</td> 
-        <td className={props.user.user_completed ? 'completed' : ''}>{props.user.user_type}</td>
-        <td>
-            <Link to={"/edituser/"+props.user._id}>Edit</Link>
-        </td> 
-        <td>
-            <Link to={"/delete/"+props.user._id}>Delete</Link> 
-        </td>
-    </tr>
-)
+export default function UsersList(){
+    const [user, setUser] = [];
 
-export default class UsersList extends Component {
+    // useEffect(() => {
+    //     axios
+    //         .get('http://localhost:4000/users/')
+    //         .then(response => {
+    //             this.setState({users: response.data});
+    //         })
+    //         .catch(function (error) {
+    //             console.log(error);
+    //         })
+    // }, []);
 
-    constructor(props) {
-        super(props);
-        this.state = {users: []};
-    }
+    const dataSource = [
+        {
+          key: '1',
+          name: 'Mike',
+          email: 'aamcc@utfpr.edu.br',
+          password: '10 Downing Street',
+          type: 'Aluno'
+        },
+        {
+          key: '2',
+          name: 'Joao',
+          email: 'joao@utfpr.edu.br',
+          password: 'porque vc mostaria a senha?',
+          type: 'Servidor'
+        },
+      ];
+      
+      const columns = [
+        {
+          title: 'Username',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'Email',
+          dataIndex: 'email',
+          key: 'email',
+        },
+        {
+            title: 'Password',
+            dataIndex: 'password',
+            key: 'password',
+          },
+          {
+            title: 'Type',
+            dataIndex: 'type',
+            key: 'Type',
+          },
+          {
+            title: 'Actions',
+            dataIndex: 'action',
+            key: 'action',        
+          render:() =>  (
+              <span>
+                  <Icon 
+                    type='edit'
+                  />
+              </span>
+          )    
+          }        
+      ];
 
-    componentDidMount() {
-        axios.get('http://localhost:4000/users/')
-            .then(response => {
-                this.setState({users: response.data});
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-    }
-
-    componentDidUpdate() {
-        axios.get('http://localhost:4000/users/')
-        .then(response => {
-            this.setState({users: response.data});
-        })
-        .catch(function (error) {
-            console.log(error);
-        })   
-    }
-
-    userList() {
-        return this.state.users.map(function(currentUser, i) {
-            return <User user={currentUser} key={i} />;
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h3>Lista de usuários</h3>
-                <table className="table table-warning table-striped" style={{ marginTop: 20 }}>
-                    <thead>
-                        <tr>
-                            <th>Username</th>
-                            <th>Email</th> 
-                            <th>Password</th> 
-                            <th>Type</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { this.userList() }
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+    
+    return (
+            <Table dataSource={dataSource} columns={columns} />
+    );
 }
