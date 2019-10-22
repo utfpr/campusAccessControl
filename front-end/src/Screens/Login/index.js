285a5a835be19aa85e405e35b87503f08a4fced3
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2019-10-22 16:11:16 -0300

    Login está pronto para uso com a API
    Só falta adicionar o link da API

--- front-end/src/Screens/Login/index.js
+++ front-end/src/Screens/Login/index.js
@@ -10,41 +10,35 @@ import {
     message, notification
 } from 'antd';
 
+const LoginForm = props => {
 
-export default function Login(props) {
+    const {getFieldDecorator, isFieldTouched, getFieldError} = props.form;
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        props.form.validateFields((err, values) => {
+            axios.post('/doLogin',{ra, senha})
+            .then(response => {
+                login(response.token, 0, response.ra);
+            })  
+            .catch((error) => {
+                notification.error({
+                    message:' Erro ',
+                    description: 'Suas credências não bateram, provavelmente é um erro humano',
+                    duration: 6
+                })
+                console.log(error);
+            })
+        })
+
+    }
 
     const [ra, setRa] = useState('');
     const [senha, setSenha] = useState('');
-    
-    const onSubmit = ()=>{
-        axios.get('http://localhost:4000/users/getuser/'+{ra, senha})
-        .then(response => {
-            login(response.token, 0, response.ra);
-        })  
-        .catch((error) => {
-            notification.error({
-                message:' Erro ',
-                description: 'Suas credências não bateram, provavelmente é um erro humano'
-            })
-            console.log(error);
-        })
-        
-    }
 
-    // render() {
-    //     const { form } = props;
-    //     const handleValuesChange = () => console.log('...');
-    //     const { useForm } = Form;
-
-    //     const form = useForm({
-    //         onValuesChange : handleValuesChange,
-    //     })
-    //     const {
-    //         getFieldDecorator, getFieldsError, getFieldError, isFieldTouched,
-    //     } = form;
-
-    //     const raError = isFieldTouched('ra') && getFieldError('ra');
-    //     const senhaError = isFieldTouched('senha') && getFieldError('senha');
+    const raError = isFieldTouched('ra') && getFieldError('ra');
+    const senhaError = isFieldTouched('senha') && getFieldError('senha');
 
         return (
             <div className={styles.holder}>
@@ -55,41 +49,43 @@ export default function Login(props) {
                         <h1>Controle de Acesso ao Campus<br />UTFPR - Campo Mourão</h1>
                         <Form>
                             <Form.Item
-                                // validateStatus={raError ? 'error' : ''}
-                                // help={raError || ''}
+                                validateStatus={raError ? 'error' : ''}
+                                help={raError || ''}
                             >
-                                {/* {getFieldDecorator('ra', {
+                                {getFieldDecorator('ra', {
                                     rules: [
                                         {
                                             required: true,
-                                            message: 'Insira seu RA!'
+                                            len: 1,
+                                            message: 'Seu R.A ou Login não é válido!'
                                         },
                                     ],
-                                })( */}
+                                })(
                                     <Input 
                                         type="text"
-                                        placeholder="RA"
+                                        placeholder="R.A ou Login"
                                         value={ra}
                                         onChange={e=>setRa(e.target.value)}
                                     />
-                                {/* )} */}
+                                )}
                             </Form.Item>
                             <Form.Item
-                                // validateStatus={senhaError ? 'error' : ''}
-                                // help={senhaError || ''}
+                                validateStatus={senhaError ? 'error' : ''}
+                                help={senhaError || ''}
                             >
-                            {/* {getFieldDecorator('senha', {
+                            {getFieldDecorator('senha', {
                                 rules: [{
                                     required: true,
-                                    message: 'Insira sua senha!'
+                                    min: 8,
+                                    message: 'Sua senha não é válida!'
                                 }],
-                            })( */}
+                            })(
                                 <Input.Password
                                     placeholder="Senha"
                                     value={senha}
                                     onChange={e=>setSenha(e.target.value)}
                                 />
-                            {/* )} */}
+                            )}
                             </Form.Item>
                             <Form.Item>
                                 <Button
@@ -97,6 +93,7 @@ export default function Login(props) {
                                     htmlType="submit"
                                     block
                                     className="btn btn-warning"
+                                    disabled = {!ra || !senha || raError || senhaError}
                                     onClick={
                                         onSubmit
                                     }
@@ -113,6 +110,6 @@ export default function Login(props) {
 
 }
 
-// const WrappedHorizontalLoginForm = Form.create({ name: 'login_form' })(LoginScreen);
+const WrappedHorizontalLoginForm = Form.create({ name: 'login' })(LoginForm);
 
-// export default WrappedHorizontalLoginForm;
+export default WrappedHorizontalLoginForm;
