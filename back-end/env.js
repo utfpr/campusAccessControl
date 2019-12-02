002406677ba5b26fb47da32a7d945f5f594e1392
MariaPinguelli <mariaferpinguelli@hotmail.com>
2019-12-02 15:59:32 -0300

    Update env.js

--- back-end/env.js
+++ back-end/env.js
@@ -1,7 +1,7 @@
 module.exports = {
-    mongoUser: 'maria',
-    mongoPassword: 'vmcyaa50',
-    mongoUrl: 'cluster0-uvjor.gcp.mongodb.net',
-    usernameAPI: 'seinfo',
-    passwordAPI: 'G7OfSOwu'
+    mongoUser: '',
+    mongoPassword: '',
+    mongoUrl: '',
+    usernameAPI: '',
+    passwordAPI: ''
 } 
